39c8ff6292fcde7268888938baf135b68a829d29
Adam Weston <adam@awcodes.com>
2023-12-25 11:51:13 -0500

    Feat: all local filament path and better output

--- app/Commands/SparkCommand.php
+++ app/Commands/SparkCommand.php
@@ -4,9 +4,11 @@ namespace App\Commands;
 
 use Illuminate\Support\Facades\Process;
 use LaravelZero\Framework\Commands\Command;
+use Throwable;
 use function Laravel\Prompts\select;
 use function Laravel\Prompts\text;
 use function Laravel\Prompts\confirm;
+use function Laravel\Prompts\spin;
 
 class SparkCommand extends Command
 {
@@ -23,6 +25,8 @@ class SparkCommand extends Command
      * @var string
      */
     protected $description = 'Set up a new reproduction repo locally.';
+
+    protected bool $hasPanels = false;
 
     /**
      * Execute the console command.
@@ -54,11 +58,22 @@ class SparkCommand extends Command
             default: 'main',
         );
 
+        $filamentPath = text(
+            label: 'Filament path',
+            default: '../filament/filament/packages/*',
+            required: true,
+        );
+
         try {
-            $this->info('Cloning repository...');
-            Process::run("git clone $repo issue-$issueNumber")->throw();
-            $this->info('Repository cloned.');
-        } catch (\Throwable $e) {
+            spin(
+                callback: function () use ($repo, $issueNumber) {
+                    Process::run("git clone $repo issue-$issueNumber")->throw();
+                },
+                message: 'Cloning repository...',
+            );
+
+            $this->components->twoColumnDetail('Clone Repository', 'DONE');
+        } catch (Throwable $e) {
             $this->error($e->getMessage());
 
             return self::FAILURE;
@@ -68,81 +83,118 @@ class SparkCommand extends Command
         chdir($workingDirectory);
 
         try {
-            $this->info('Installing dependencies...');
-            Process::run("composer install")->throw();
-            $this->info('Dependencies installed.');
-        } catch (\Throwable $e) {
-            $this->error($e->getMessage());
-
-            return self::FAILURE;
-        }
-
-        try {
-            $this->info('Checking out branch...');
-            Process::run("git checkout $branch")->throw();
-            $this->info('Branch checked out.');
-        } catch (\Throwable $e) {
-            $this->error($e->getMessage());
-
-            return self::FAILURE;
-        }
-
-        $composer = json_decode(file_get_contents($workingDirectory . '/composer.json'), true);
-        $hasPanels = array_key_exists('filament/filament', $composer['require'])
-            || array_key_exists('filament/filament', $composer['require-dev']);
-
-        try {
-            $this->info('Setting up repo...');
-            Process::run("cp .env.example .env")->throw();
-            Process::run("php artisan key:generate")->throw();
-            Process::run("touch database/database.sqlite")->throw();
-
-            $this->replaceInFile(
-                $workingDirectory . '/.env',
-                [
-                    'DB_CONNECTION=mysql' => 'DB_CONNECTION=sqlite',
-                    'DB_DATABASE=' => '#DB_DATABASE=',
-                ]);
-
-            Process::run("php artisan migrate")->throw();
-            Process::run("php artisan db:seed")->throw();
-
-            if ($hasPanels) {
-                Process::run("php artisan make:filament-user --name=test --email=test@example.com --password=password")
-                    ->throw();
-            }
-
-            Process::run('npm install')->throw();
-            Process::run('npm run build')->throw();
-
-            $openInEditor = confirm(
-                label: 'Open in editor?',
-                default: true,
+            spin(
+                callback: function () use ($workingDirectory, $filamentPath) {
+                    $this->modifyComposer($workingDirectory, $filamentPath);
+                },
+                message: 'Modifying composer.json...',
             );
 
-            if ($openInEditor) {
-                $editor = select(
-                    label: 'Editor',
-                    options: [
-                        'VS Code',
-                        'PHPStorm (phpstorm)',
-                        'PHPStorm Alt (pstorm)',
-                    ],
-                    default: 'VS Code',
-                );
-
-                $editor = match ($editor) {
-                    'VS Code' => 'code',
-                    'PHPStorm (phpstorm)' => 'phpstorm',
-                    'PHPStorm Alt (pstorm)' => 'pstorm',
-                };
-
-                Process::run("$editor .")->throw();
-            }
-        } catch (\Throwable $e) {
+            $this->components->twoColumnDetail('Modify composer.json', 'DONE');
+        } catch (Throwable $e) {
             $this->error($e->getMessage());
 
             return self::FAILURE;
+        }
+
+        try {
+            spin(
+                callback: function () {
+                    Process::run("composer install")->throw();
+                },
+                message: 'Installing composer dependencies...',
+            );
+
+            $this->components->twoColumnDetail('Install composer dependencies', 'DONE');
+        } catch (Throwable $e) {
+            $this->error($e->getMessage());
+
+            return self::FAILURE;
+        }
+
+        try {
+            spin(
+                callback: function () use ($branch) {
+                    Process::run("git checkout $branch")->throw();
+                },
+                message: 'Checking out branch...',
+            );
+
+            $this->components->twoColumnDetail('Checkout branch', 'DONE');
+        } catch (Throwable $e) {
+            $this->error($e->getMessage());
+
+            return self::FAILURE;
+        }
+
+        try {
+
+            spin(
+                callback: function () use ($workingDirectory) {
+                    Process::run("cp .env.example .env")->throw();
+                    Process::run("php artisan key:generate")->throw();
+                    Process::run("touch database/database.sqlite")->throw();
+
+                    $this->replaceInFile(
+                        $workingDirectory . '/.env',
+                        [
+                            'DB_CONNECTION=mysql' => 'DB_CONNECTION=sqlite',
+                            'DB_DATABASE=' => '#DB_DATABASE=',
+                        ]);
+
+                    Process::run("php artisan migrate")->throw();
+                    Process::run("php artisan db:seed")->throw();
+
+                    if ($this->hasPanels) {
+                        try {
+                            Process::run("php artisan make:filament-user --name=test --email=test@example.com --password=password")
+                                ->throw();
+                        } catch (Throwable) {}
+                    }
+                },
+                message: 'Setting up repo...',
+            );
+
+            $this->components->twoColumnDetail('Setup repository', 'DONE');
+
+            spin(
+                callback: function () use ($workingDirectory) {
+                    Process::run('npm install')->throw();
+                    Process::run('npm run build')->throw();
+                },
+                message: 'Installing npm dependencies...',
+            );
+
+            $this->components->twoColumnDetail('Install npm dependencies', 'DONE');
+        } catch (Throwable $e) {
+            $this->error($e->getMessage());
+
+            return self::FAILURE;
+        }
+
+        $openInEditor = confirm(
+            label: 'Open in editor?',
+            default: true,
+        );
+
+        if ($openInEditor) {
+            $editor = select(
+                label: 'Editor',
+                options: [
+                    'VS Code',
+                    'PHPStorm (phpstorm)',
+                    'PHPStorm Alt (pstorm)',
+                ],
+                default: 'VS Code',
+            );
+
+            $editor = match ($editor) {
+                'VS Code' => 'code',
+                'PHPStorm (phpstorm)' => 'phpstorm',
+                'PHPStorm Alt (pstorm)' => 'pstorm',
+            };
+
+            Process::run("$editor .");
         }
 
         $this->info('Repro is ready 🦒');
@@ -165,5 +217,50 @@ class SparkCommand extends Command
                 $contents
             )
         );
+    }
+
+    private function modifyComposer(string $workingDirectory, string $filamentPath): void
+    {
+        $contents = json_decode(file_get_contents($workingDirectory . '/composer.json'), true);
+
+        $packages = [
+            'filament/filament',
+            'filament/forms',
+            'filament/tables',
+            'filament/notifications',
+            'filament/infolists',
+            'filament/widgets',
+            'filament/actions',
+        ];
+
+        foreach ($contents['require'] as $name => $version) {
+            if (in_array($name, $packages, true)) {
+                $contents['require'][$name] = '*';
+            }
+        }
+
+        foreach ($contents['require-dev'] as $name => $version) {
+            if (in_array($name, $packages, true)) {
+                $contents['require-dev'][$name] = '*';
+            }
+        }
+
+        $contents['repositories'] = [
+            [
+                'type' => 'path',
+                'url' => $filamentPath,
+            ],
+        ];
+
+        $contents['minimum-stability'] = 'dev';
+
+        if (
+            array_key_exists('filament/filament', $contents['require'])
+            || array_key_exists('filament/filament', $contents['require-dev'])
+        ) {
+            $this->hasPanels = true;
+        }
+
+        file_put_contents($workingDirectory . '/composer.json', json_encode($contents, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE));
     }
 }
